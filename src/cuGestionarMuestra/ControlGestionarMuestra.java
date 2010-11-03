551835dee2cd8c795c3e5810a63279faa9ee9c0f
marmolleonardo <marmolleonardo@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-03 13:27:10 +0000

    controlGestionarMuestra mas completa.

--- src/cuGestionarMuestra/ControlGestionarMuestra.java
+++ src/cuGestionarMuestra/ControlGestionarMuestra.java
@@ -3,10 +3,57 @@
  */
 package cuGestionarMuestra;
 
+import persistencia.Persistencia;
+
 /**
- * @author NAVE
- *
+ * @author tesisGeologia
+ * @version 1.0
  */
 public class ControlGestionarMuestra {
 
+	/**
+	 * Inserta una muestra con persistencia. 
+	 */ 
+	public void insertarMuestra(Muestra mu) throws Exception{
+		Persistencia persistencia = new Persistencia();
+		try {
+			persistencia.insertarObjeto(mu);
+			persistencia.cierraTransaccion();
+			System.out.println("Muestra insertada con persistencia");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+	}
+	
+	/**
+	 * Elimina una muestra con persistencia. 
+	 */
+	public void eliminarMuestra(Muestra mu) throws Exception {
+		Persistencia persistencia = new Persistencia();
+		try {
+			persistencia.eliminarObjeto(mu);
+			persistencia.cierraTransaccion();
+			System.out.println("Muestra eliminada con persistencia");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+	}
+	
+	/**
+	 * Elimina una muestra con persistencia. 
+	 */
+	public void modificarMuestra(Muestra mu) throws Exception {
+		Persistencia persistencia = new Persistencia();
+		try {
+			persistencia.eliminarObjeto(mu);
+			persistencia.cierraTransaccion();
+			System.out.println("Muestra modificada con persistencia");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+	}
+	
+	
 }
+	
+	
