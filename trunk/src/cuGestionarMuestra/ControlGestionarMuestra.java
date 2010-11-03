11ed7fabcfdafa33bc07fef66863e8de42aaaf37
marmolleonardo <marmolleonardo@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-03 22:04:39 +0000

    Control mas Completo

---
+++ trunk/src/cuGestionarMuestra/ControlGestionarMuestra.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package cuGestionarMuestra;
+
+import java.util.Collection;
+
+import persistencia.Persistencia;
+
+/**
+ * @author tesisGeologia
+ * @version 1.0
+ */
+public class ControlGestionarMuestra {
+
+	/**
+	 * Inserta una muestra con persistencia. 
+	 */ 
+	public void insertarMuestra(Muestra mu) throws Exception{
+		Persistencia persistencia = new Persistencia();
+		try {
+			persistencia.insertarObjeto(mu);
+			persistencia.cierraTransaccion();
+			System.out.println("Muestra insertada con persistencia");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+	}
+	
+	/**
+	 * Elimina una muestra con persistencia. 
+	 */
+	public void eliminarMuestra(Muestra mu) throws Exception {
+		Persistencia persistencia = new Persistencia();
+		try {
+			persistencia.eliminarObjeto(mu);
+			persistencia.cierraTransaccion();
+			System.out.println("Muestra eliminada con persistencia");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+	}
+			
+	/**
+	 * Retorna todos los elementos de la case pasada como persistente
+	 */
+	public Collection coleccionMuestras(Class clase) throws Exception {
+		Collection<Object> aux = null; 
+		Persistencia persistencia = new Persistencia();
+		try {
+			aux = (persistencia.buscarColeccion(clase));
+			persistencia.cierraTransaccion();
+			System.out.println("Muestra modificada con persistencia");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+		return aux;
+	}
+	
+	
+}
+	
+	
