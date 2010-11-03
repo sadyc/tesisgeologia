d81f70cc584681d966c345eb56c93d53096b2138
marmolleonardo <marmolleonardo@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-03 00:13:40 +0000

    CU Clasificacion . Clases persistente empezadas

---
+++ src/cuCalcularClasificacion/Clasificaciones/AASHTO.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package cuCalcularClasificacion.Clasificaciones;
+
+import cuCalcularClasificacion.Clasificacion;
+
+/**
+ * @author TesisGeologia.
+ * @version 1.0.
+ */
+public class AASHTO extends Clasificacion{
+
+	/**
+	 * @brief Constructor por defecto.
+	 */
+	public AASHTO() {
+		descripcion = "arena de metales";
+	}
+	
+	/**
+	 * @param descripcion
+	 */
+	public AASHTO(String descripcion) {
+		super(descripcion);
+		// TODO Auto-generated constructor stub
+	}
+
+}
