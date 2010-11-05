3bb51bec5a8c224d2cc92bd080afcc35cbc6ed84
manuvarel <manuvarel@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-05 23:58:40 +0000

    comenzando el gestionar analisis

--- src/cuGestionarAnalisis/Main.java
+++ src/cuGestionarAnalisis/Main.java
@@ -8,6 +8,15 @@ public class Main {
 	public static void main(String[] args) {
 		MediadorGestionarAnalisis probando = new MediadorGestionarAnalisis();
 		probando.show();
+		GUIGestionarAnalisis hola;
+		try {
+			hola = new GUIGestionarAnalisis("hola chicos");
+			hola.show();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
