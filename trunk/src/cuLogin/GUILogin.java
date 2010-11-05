ac02e2838d2d722ab9993076f174961819d3c848
manuvarel <manuvarel@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-05 03:00:43 +0000

    la ventanita del login perfecta!!

--- trunk/src/cuLogin/GUILogin.java
+++ trunk/src/cuLogin/GUILogin.java
@@ -3,7 +3,14 @@
  */
 package cuLogin;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -12,14 +19,159 @@ import javax.swing.JTextField;
  * @author TesisGeologia
  *
  */
-public class GUILogin {
+public class GUILogin extends JFrame{
 	
 	private JButton aceptar;
 	private JButton cancelar;
-	private JPanel panelNorte=null;
-	private JPanel panelSur=null;
+	private JPanel panelNorte;
+	private JPanel panelSur;
 	private JTextField nombre;
 	private JTextField password;
 	private JLabel nombreLabel;
 	private JLabel passwordLabel;
+	
+	
+	
+	/**
+	 * @param aceptar
+	 * @param cancelar
+	 * @param panelNorte
+	 * @param panelSur
+	 * @param nombre
+	 * @param password
+	 * @param nombreLabel
+	 * @param passwordLabel
+	 */
+	public GUILogin(String title) {
+		super(title);
+		aceptar = new JButton ("Aceptar");
+		cancelar = new JButton ("Cancelar");
+		panelNorte = null;
+		panelSur = null;
+		nombre = new JTextField(15);
+		password = new JTextField(15);
+		nombreLabel = new JLabel ("Nombre Usuario: ");
+		passwordLabel = new JLabel ("Password: ");
+		initialize();
+	}
+	/**
+	 * @return the aceptar
+	 */
+	public JButton getJButtonAceptar() {
+		return aceptar;
+	}
+	/**
+	 * @param aceptar the aceptar to set
+	 */
+	public void setJButtonAceptar(JButton aceptar) {
+		this.aceptar = aceptar;
+	}
+	/**
+	 * @return the cancelar
+	 */
+	public JButton getJButtonCancelar() {
+		return cancelar;
+	}
+	/**
+	 * @param cancelar the cancelar to set
+	 */
+	public void setJButtonCancelar(JButton cancelar) {
+		this.cancelar = cancelar;
+	}
+	/**
+	 * @return the panelNorte
+	 */
+	public JPanel getPanelNorte() {
+		if (this.panelNorte==null) {
+			this.panelNorte= new JPanel();
+			this.panelNorte.setLayout(new BoxLayout(this.panelNorte,BoxLayout.Y_AXIS));
+			nombreLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+			nombre.setAlignmentX(Component.CENTER_ALIGNMENT);
+			passwordLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+			password.setAlignmentX(Component.CENTER_ALIGNMENT);
+			this.panelNorte.add(nombreLabel);
+			this.panelNorte.add(nombre);
+			this.panelNorte.add(passwordLabel); 
+			this.panelNorte.add(password);
+		}
+		return this.panelNorte;
+	}
+
+	/**
+	 * @return the panelSur
+	 */
+	public JPanel getPanelSur() {
+		if (this.panelSur==null) {
+			this.panelSur = new JPanel();
+			this.panelSur.setLayout(new FlowLayout());
+			this.panelSur.add(aceptar);
+			this.panelSur.add(cancelar);
+		}
+		return this.panelSur;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public JTextField getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(JTextField nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the password
+	 */
+	public JTextField getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(JTextField password) {
+		this.password = password;
+	}
+	
+	/**
+	 * @param nombreLabel the nombreLabel to set
+	 */
+	public void setNombreLabel(JLabel nombreLabel) {
+		this.nombreLabel = nombreLabel;
+	}
+	
+	/**
+	 * @param passwordLabel the passwordLabel to set
+	 */
+	public void setPasswordLabel(JLabel passwordLabel) {
+		this.passwordLabel = passwordLabel;
+	}
+	
+	/**
+	 * Metodo que inicializa la interfaz.
+	 *
+	 * @return void
+	 */
+	private  void initialize() {
+		this.setSize(300 , 300);
+        // Seteamos el BorderLayout
+		this.getContentPane().setLayout(new BorderLayout()); 		
+	 	// Se aaden los componentes al Frame
+		// Agregamos el Panel Norte al Frame
+	 	this.getContentPane().add(this.getPanelNorte(),BorderLayout.NORTH);
+	 	// Agregamos el Panel Sur al Frame
+	 	this.getContentPane().add(this.getPanelSur(),BorderLayout.SOUTH);
+	}
+	
+	/**
+	 * Metodo que permite escuchar los botoner aceptar y cancelar.
+	 *
+	 *@param lis actionEvent asignado a los botones.
+	 */
+	public void setListenerButtons(ActionListener lis){
+		this.aceptar.addActionListener(lis);
+		this.cancelar.addActionListener(lis);
+	}
 }
---
+++ trunk/src/cuLogin/MediadorLogin.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package cuLogin;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author TesisGeologia
+ *
+ */
+public class MediadorLogin implements ActionListener{
+	
+	private GUILogin login = null;
+	private Component frame;
+	
+	/**
+	 * Constructor con parametros
+	 * @param nombreVentana
+	 * @throws Exception
+	 */
+	public MediadorLogin (String nombreVentana) throws Exception {
+		super();
+		this.login = new GUILogin(nombreVentana);
+		this.login.setListenerButtons(this);
+	}
+	
+	
+	public void show()	{
+		 login.show();
+	}
+	
+	/**
+	 * Metodo que necesita definir al implementar la interface ActionListener 
+	 * Para tratar los eventos de acciones de los componentes 
+	 */
+	public void actionPerformed(ActionEvent arg0) {
+		Object source = arg0.getSource();
+		if (this.login.getJButtonAceptar() == source){
+	   		System.out.println("GestionarMediador.actionPerformed() jButtonAgregar");
+	   		if (login.getNombre().getText().equals("") ){
+				JOptionPane.showMessageDialog(frame,"Debe completar el campo de 'Nombre Usuario'","ERROR!!!!!!!!!", JOptionPane.ERROR_MESSAGE);
+			}
+			else {
+				if (login.getPassword().getText().equals("")) {
+					JOptionPane.showMessageDialog(frame,"Debe completar el campo de 'Password'","ERROR!!!!!!!!!", JOptionPane.ERROR_MESSAGE);
+				}
+				else{
+					login.dispose();
+				}
+			}
+		}
+		if (this.login.getJButtonCancelar() == source){
+	   		System.out.println("GestionarMediador.actionPerformed() jButtonCancelar");
+	   		login.dispose();	
+	   	}
+	}
+}
