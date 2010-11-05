178a9138efbadc381a44945191d32602344614e1
manuvarel <manuvarel@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-05 00:12:46 +0000

    nuevo acomodamiento despues del reto 04/11

---
+++ trunk/src/cuLogin/GUILogin.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package cuLogin;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * @author TesisGeologia
+ *
+ */
+public class GUILogin {
+	
+	private JButton aceptar;
+	private JButton cancelar;
+	private JPanel panelNorte=null;
+	private JPanel panelSur=null;
+	private JTextField nombre;
+	private JTextField password;
+	private JLabel nombreLabel;
+	private JLabel passwordLabel;
+}
