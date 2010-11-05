e346f47721b42387dfd67ec01d63b1c3df023274
manuvarel <manuvarel@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-05 19:40:16 +0000

    hecha la ventanita de UN tamiz

---
+++ trunk/src/cuGestionarTamiz/GUITamiz.java
@@ -0,0 +1,169 @@
+/**
+ * 
+ */
+package cuGestionarTamiz;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import persistencia.domain.Tamiz;
+
+/**
+ * @author TesisGeologia
+ *
+ * Clase que define la interfaz para manipular un Analisis
+ */
+public class GUITamiz extends JDialog {
+
+	/**
+	 * @param title
+	 * @throws java.awt.HeadlessException
+	 */
+	private JButton aceptar;
+	private JButton cancelar;
+	private JPanel panelNorte=null;
+	private JPanel panelSur=null;
+	private JTextField numeroTamiz;
+	private JTextField aberturaMalla;
+	private JLabel numeroTamizLabel;
+	private JLabel aberturaMallaLabel;
+
+	/**
+	 * This is the default constructor
+	 */
+	public GUITamiz() {
+		super();
+		numeroTamiz = new JTextField(15);
+		aberturaMalla = new JTextField(15);
+		numeroTamizLabel = new JLabel ("Numero de Tamiz: ");
+		aberturaMallaLabel = new JLabel ("Abertura de Malla: ");
+		aceptar = new JButton("Agregar Tamiz");
+		cancelar = new JButton("Cancelar");
+		initialize();
+	}
+	
+	/**
+	 * This is the parametrized constructor used in modification
+	 * @param
+	 */
+	public GUITamiz(Tamiz tamiz) {
+		super();
+		numeroTamiz = new JTextField(15);
+		aberturaMalla = new JTextField(15);
+		numeroTamiz.setText(tamiz.getNumeroTamiz().toString());
+		aberturaMalla.setText(tamiz.getAberturaMalla().toString());
+		numeroTamizLabel = new JLabel ("Numero de Tamiz: ");
+		aberturaMallaLabel = new JLabel ("Abertura de Malla: ");
+		aceptar = new JButton("Modificar Tamiz");
+		cancelar = new JButton("Cancelar");
+		initialize();
+	}
+	
+	/**
+	 * @return the numero tamiz.
+	 */
+	public JTextField getNumeroTamiz() {
+		return numeroTamiz;
+	}
+
+	/**
+	 * @return the abertura de malla.
+	 */
+	public JTextField getAberturaMalla() {
+		return aberturaMalla;
+	}
+
+	/**
+	 * @return the aceptar
+	 */
+	public JButton getJButtonAceptar() {
+		return aceptar;
+	}
+
+	/**
+	 * @param aceptar the aceptar to set
+	 */
+	public void setJButtonAceptar(JButton aceptar) {
+		this.aceptar = aceptar;
+	}
+	
+	/**
+	 * @return the cancelar
+	 */
+	public JButton getJButtonCancelar() {
+		return cancelar;
+	}
+
+	/**
+	 * @param cancelar the cancelar to set
+	 */
+	public void setJButtonCancelar(JButton cancelar) {
+		this.cancelar = cancelar;
+	}
+
+	/**
+	 * Metodo que inicializa la interfaz.
+	 *
+	 * @return void
+	 */
+	private  void initialize() {
+		this.setSize(300 , 300);
+       	this.getContentPane().setLayout(new BorderLayout()); 		
+	 	this.getContentPane().add(this.getPanelNorte(),BorderLayout.NORTH);
+	 	this.getContentPane().add(this.getPanelSur(),BorderLayout.SOUTH);
+	}
+   
+	
+	/**
+	 * Metodo que retorna el panelNorte.
+	 *
+	 * @return Jpanel
+	 */
+		
+	public JPanel getPanelNorte() {
+		if (this.panelNorte==null) {
+			this.panelNorte= new JPanel();
+			this.panelNorte.setLayout(new BoxLayout(this.panelNorte,BoxLayout.Y_AXIS));
+			numeroTamiz.setAlignmentX(Component.CENTER_ALIGNMENT);
+			aberturaMalla.setAlignmentX(Component.CENTER_ALIGNMENT);
+			this.panelNorte.add(numeroTamizLabel);
+			this.panelNorte.add(numeroTamiz);
+			this.panelNorte.add(aberturaMallaLabel);
+			this.panelNorte.add(aberturaMalla);
+		}
+		return this.panelNorte;
+	}	
+
+	/**
+	 * Metodo que retorna el panelSur.
+	 *
+	 * @return Jpanel
+	 */
+	public JPanel getPanelSur() {
+		if (this.panelSur==null) {
+			this.panelSur = new JPanel();
+			this.panelSur.setLayout(new FlowLayout());
+			this.panelSur.add(aceptar);
+			this.panelSur.add(cancelar);
+			}
+		return this.panelSur;
+	}
+	
+	/**
+	 * Metodo que permite escuchar los botoner aceptar y cancelar.
+	 *
+	 *@param lis actionEvent asignado a los botones.
+	 */
+	public void setListenerButtons(ActionListener lis){
+		this.aceptar.addActionListener(lis);
+		this.cancelar.addActionListener(lis);
+	}
+}
---
+++ trunk/src/cuGestionarTamiz/MediadorTamiz.java
@@ -0,0 +1,100 @@
+package cuGestionarTamiz;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import persistencia.domain.Tamiz;
+
+
+
+/**
+ * @author TesisGeologia
+ * Implementa las interfaces de acuerdo a los eventos que necesita tratar
+ * en este caso: ActionListener,MouseListener,ItemListener.
+ */
+
+public class MediadorTamiz  implements ActionListener,MouseListener,ItemListener  {
+	
+	private GUITamiz tamiz;
+	
+	/**
+	 * This is the default constructor
+	 */
+	public MediadorTamiz() {
+		super();
+		tamiz = new GUITamiz();
+		tamiz.setTitle("Tamiz");
+		tamiz.setModal(true);
+		tamiz.setListenerButtons(this);
+		show();
+	}
+		
+	/**
+	 * This is the default constructor
+	 */
+	public MediadorTamiz(Tamiz tamiz) {
+		super();
+		this.tamiz = new GUITamiz(tamiz);
+		this.tamiz.setTitle("Tamiz");
+		this.tamiz.setModal(true);
+		this.tamiz.setListenerButtons(this);
+		show();
+	}
+	
+	public void show(){
+		tamiz.show();
+	}
+	
+	/**
+	 * @return the altaMuestra
+	 */
+	public GUITamiz getTamiz() {
+		return tamiz;
+	}	
+	
+	/**
+	 * Metodo que necesita definir al implementar la interface ActionListener 
+	 * Para tratar los eventos de acciones de los componentes 
+	 */
+	public void actionPerformed(ActionEvent arg0) {
+		Object source = arg0.getSource();
+     	if (this.tamiz.getJButtonAceptar() == source){
+			System.out.println("GestionarTamiz.actionPerformed() jButtonAceptarTamiz");
+			tamiz.dispose();
+		}
+		if (this.tamiz.getJButtonCancelar() == source){
+			System.out.println("GestionarTamiz.actionPerformed() jButtonCancelar");
+			tamiz.dispose();
+		}
+	}
+
+
+
+	public void itemStateChanged(ItemEvent arg0) {
+	}
+
+
+	public void mouseClicked(MouseEvent arg0) {
+	}
+
+
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+
+	public void mousePressed(MouseEvent arg0) {
+	}
+
+
+	public void mouseReleased(MouseEvent arg0) {
+	}
+}
