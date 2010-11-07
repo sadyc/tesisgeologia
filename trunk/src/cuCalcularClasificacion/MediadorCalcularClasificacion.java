b984f251683a2121a90ece57fe3cf186b3203d59
fcastellina <fcastellina@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-07 23:02:55 +0000

    GUIClasificacion, Mediador empezado

--- trunk/src/cuCalcularClasificacion/MediadorCalcularClasificacion.java
+++ trunk/src/cuCalcularClasificacion/MediadorCalcularClasificacion.java
@@ -3,10 +3,97 @@
  */
 package cuCalcularClasificacion;
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JOptionPane;
+
+import persistencia.domain.Clasificacion;
+import persistencia.domain.Muestra;
+import persistencia.domain.OperadorDeLaboratorio;
+import persistencia.domain.Ubicacion;
+import persistencia.domain.Usuario;
+import cuGestionarMuestra.ControlGestionarMuestra;
+import cuGestionarMuestra.GUIMuestra;
+
 /**
- * @author NAVE
+ * @brief Clase que se utiliza para escuchar los sucesos que suceden en la ventana de clasificacion
+ * @author TesisGeologia
  *
  */
-public class MediadorCalcularClasificacion {
+public class MediadorCalcularClasificacion implements ActionListener,MouseListener,ItemListener{
+	private GUIClasificacion GUIClasificacion;
+	private String[] data = new String [9];
+	private Component frame;
 
+	public MediadorCalcularClasificacion() throws Exception {
+		super();
+		this.GUIClasificacion = new GUIClasificacion();
+		GUIClasificacion.setTitle("Clasificacion de la  Muestra");
+		GUIClasificacion.setModal(true);
+		
+		// se configura como escuchador de los evenetos de la ventana 
+		// al el mismo (mediador)
+		this.GUIClasificacion.setListenerButtons(this);
+		GUIClasificacion.show();
+	}
+	
+
+	@Override
+	public void itemStateChanged(ItemEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Object source = arg0.getSource();
+		//ControlGestionarMuestra control = new ControlGestionarMuestra();
+		if (this.GUIClasificacion.getJButtonAceptar() == source) {
+			GUIClasificacion.dispose();
+				
+			}
+		if (this.GUIClasificacion.getJButtonCancelar() == source){
+			GUIClasificacion.dispose();
+		}
+	}
+	public String[] getData() {
+		return data;
+	}
 }
+
