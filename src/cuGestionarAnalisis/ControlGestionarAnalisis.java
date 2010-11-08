ce8d692686b115ebda2fceb1f26fc7649cfedb3a
manuvarel <manuvarel@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-11-08 04:53:12 +0000

    realizado la clase de control del "gestionar analisis"

---
+++ src/cuGestionarAnalisis/ControlGestionarAnalisis.java
@@ -0,0 +1,65 @@
+package cuGestionarAnalisis;
+
+import java.util.Collection;
+import persistencia.Persistencia;
+import persistencia.domain.Analisis;
+
+/**
+ * @author tesisGeologia
+ * @version 1.0
+ */
+public class ControlGestionarAnalisis {
+
+	
+	/**
+	 * Contructor por defecto
+	 */
+	public ControlGestionarAnalisis(){}
+	
+	/**
+	 * Inserta un analisis con persistencia. 
+	 */ 
+	public void insertarAnalisis(Analisis analisis) throws Exception{
+		Persistencia persistencia = new Persistencia();
+		try {
+			persistencia.insertarObjeto(analisis);
+			persistencia.cierraTransaccion();
+			System.out.println("Analisis insertado con persistencia");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+	}
+	
+	/**
+	 * Elimina un analisis persistente. 
+	 */
+	public void eliminarAnalisis(Analisis analisis) throws Exception {
+		Persistencia persistencia = new Persistencia();
+		try {
+			persistencia.eliminarObjeto(analisis);
+			persistencia.cierraTransaccion();
+			System.out.println("Analisis eliminado con persistencia");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+	}
+			
+	/**
+	 * Retorna todos los elementos persistentes de la clase pasada como parametro.
+	 */
+	public Collection coleccionAnalisis(Class clase) throws Exception {
+		Collection<Object> aux = null; 
+		Persistencia persistencia = new Persistencia();
+		try {
+			aux = (persistencia.buscarColeccion(clase));
+			persistencia.cierraTransaccion();
+			System.out.println("analisis coleccionados");
+		} catch (Exception e) {
+			persistencia.realizarRollback();
+		}
+		return aux;
+	}
+	
+	
+}
+	
