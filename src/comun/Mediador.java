23e9bacfdb4aae6fcc163322f877caa131b41735
marmolleonardo <marmolleonardo@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2010-12-10 23:21:11 +0000

    Mediador

---
+++ src/comun/Mediador.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package comun;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Vector;
+
+/**
+ * Clase que se utiliza para que cada mediador extienda a esta clase. 
+ * @author TesisGeologia.
+ */
+abstract class Mediador implements ActionListener,MouseListener,ItemListener{
+	
+	public void init(){
+	}
+
+	public void show(){
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+	}
+
+	public void mouseEntered(MouseEvent arg0) {    
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+
+	}
+
+	public String[] getData(){
+		return null;
+	}
+}
