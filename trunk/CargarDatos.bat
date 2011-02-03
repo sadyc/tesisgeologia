039ecc3c847f2567127fc80d319597bf3aa25bd3
marmolleonardo <marmolleonardo@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2011-02-03 17:38:43 +0000

    Ejecucion

--- trunk/CargarDatos.bat
+++ trunk/CargarDatos.bat
@@ -7,21 +7,20 @@ set dlib=%dpro%\lib
 cd classes
 
 set classpath=.
-set classpath=%classpath%;%dlib%\datanucleus-enhancer-2.1.0-release.jar
 set classpath=%classpath%;%dlib%\jdo2-api-2.3-eb.jar
 set classpath=%classpath%;%dlib%\datanucleus-core-2.2.0-m1.jar
+set classpath=%classpath%;%dlib%\datanucleus-rdbms-2.2.0-m1.jar
 set classpath=%classpath%;%dlib%\log4j-1.2.7.jar
-set classpath=%classpath%;%dlib%\asm-3.1.jar
+set classpath=%classpath%;%dlib%\mysql-connector-java-5.1.12-bin.jar
 set classpath=%classpath%;%dlib%\jfreechart-1.0.13.jar
 set classpath=%classpath%;%dlib%\jcommon-1.0.16.jar
+
 set classpath=%classpath%;%dpro%\classes
 
-rem ejecuta el enhancer de los .class de la persistencia
-
 cd..
+rem ejecuta 
 
-%djav%\bin\java -cp %classpath%  -Dlog4j.configuration=file:log4j.properties org.datanucleus.enhancer.DataNucleusEnhancer %dpro%\classes\persistencia\domain\*.class
-
+%djav%\bin\java -cp %classpath%  persistencia.domain.CargaDatosMain
 
 pause
 
