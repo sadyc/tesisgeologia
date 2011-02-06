ba47f51cdf85d2a1ccac0ef9f45d9b316b6b2e49
fcastellina <fcastellina@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2011-02-06 23:01:13 +0000

    todas las librerias y todo

--- Ejecutar.bat
+++ Ejecutar.bat
@@ -1,6 +1,6 @@
 cls
 set dpro=%cd%
-set djav="C:\Program Files\Java\jre1.6.0_20"
+set djav="C:\Program Files\Java\jdk1.6.0_23"
 set dlib=%dpro%\lib
 
 
@@ -12,6 +12,9 @@ set classpath=%classpath%;%dlib%\datanucleus-core-2.2.0-m1.jar
 set classpath=%classpath%;%dlib%\datanucleus-rdbms-2.2.0-m1.jar
 set classpath=%classpath%;%dlib%\log4j-1.2.7.jar
 set classpath=%classpath%;%dlib%\mysql-connector-java-5.1.12-bin.jar
+set classpath=%classpath%;%dlib%\jfreechart-1.0.13.jar
+set classpath=%classpath%;%dlib%\jcommon-1.0.16.jar
+
 set classpath=%classpath%;%dpro%\classes
 
 cd..
--- src/cuReporte/report/ViewReport.java
+++ src/cuReporte/report/ViewReport.java
@@ -7,7 +7,7 @@ import net.sf.jasperreports.view.*;
 
 public class ViewReport {
 	
-	private static String[] []valores;
+	private static String[][] valores;
     
     public ViewReport(String[][] valores){
             this.valores = valores.clone();
@@ -28,7 +28,20 @@ public class ViewReport {
 			for (int i = 0; i < fieldXml.length; i++) {
 				ht.put(fieldXml[i], new Integer(i));
 			}
-			
+			System.out.println(ht+" <---- HT");
+			System.out.println(values[0][0]+" <---- VALUES");
+			System.out.println(values[0][1]+" <---- VALUES");
+			System.out.println(values[0][2]+" <---- VALUES");
+			System.out.println(values[0][3]+" <---- VALUES");
+			System.out.println(values[0][4]+" <---- VALUES");
+			System.out.println(values[0][5]+" <---- VALUES");
+			System.out.println(values[0][6]+" <---- VALUES");
+			System.out.println(values[0][7]+" <---- VALUES");
+			System.out.println(values[0][8]+" <---- VALUES");
+			System.out.println(values[0][9]+" <---- VALUES");
+			System.out.println(values[0][10]+" <---- VALUES");
+			System.out.println(values[0][11]+" <---- VALUES");
+			System.out.println(values[0][12]+" <---- VALUES");
 			DataSourceJasper data = new DataSourceJasper(values, ht);
 			String fileNameReport = MakeReport.PATH_OUTPUT_REPORT + nameReport + ".jasper";
 			System.out.println("Reporte:" + fileNameReport);
@@ -46,8 +59,8 @@ public class ViewReport {
 		
 		//parameters
 		//Object[][] parameters = { { "reportTitle", "Listado de Personas." },{ "datePrint", "11-11-05" } };
-		Object[][] parameters = { { "datePrint", "11-11-07" },{ "reportTitle", "Listado de Personas." },{ "datePrint", "11-11-07" },{ "reportTitle", "Listado de Personas." } };
-     	String[] filedXml = { "nombre", "ubicacion", "peso" ,"profindidadInicial", "profundidadFinal", "limiteLiquido", "limitePlastico", "indicePlasticidad", "D60", "D30" , "D10", "coefUniformidad", "gradoCurvatura"};   //Son los campos a llenar en el XML. Deben tener el mismo nombre.
+		Object[][] parameters = { { "datePrint", "11-11-07" },{ "reportTitle", "Listado de Personas." } };
+     	String[] filedXml = { "nombre", "ubicacion", "peso", "profindidadInicial", "profundidadFinal", "limiteLiquido", "limitePlastico", "indicePlasticidad", "D60", "D30" , "D10", "coefUniformidad", "gradoCurvatura"};   //Son los campos a llenar en el XML. Deben tener el mismo nombre.
  
 		String nameReport = "report1";
 
