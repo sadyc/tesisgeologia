e0abc6fc23646b7b2316acebc32ac4b78aa970de
manuvarel <manuvarel@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2011-02-18 23:27:09 +0000

    revisa si es windows de 32 o 64 bits

--- trunk/src/comun/MediadorPrincipal.java
+++ trunk/src/comun/MediadorPrincipal.java
@@ -120,7 +120,7 @@ public class MediadorPrincipal extends Mediador{
 			GUIPrincipal.dispose();
 		}
 		if (this.GUIPrincipal.getjButtonCrearBackup()==source || this.GUIPrincipal.getCrearBackupMenu()==source){
-			backup.crearBackup();
+			backup.crearBackUpWindowsX86();
 		}
 		if (this.GUIPrincipal.getjButtonCargarBackup()==source || this.GUIPrincipal.getCargarBackupMenu()==source){
 			backup.cargarBackup();
