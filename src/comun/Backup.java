c4640d51c7cd34ed96f409c4452089c9cd14c156
manuvarel <manuvarel@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2011-02-19 15:45:08 +0000

    back-up arreglado definitivamente! y achicada la letra del reporte..

--- src/comun/Backup.java
+++ src/comun/Backup.java
@@ -9,11 +9,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.Date;
+import java.sql.DriverManager;
 
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
 /**
 * @author TesisGeología
@@ -26,8 +30,28 @@ public class Backup {
     //para guardar el archivo SQL
     private FileWriter  fichero = null;
     private PrintWriter pw = null;
+    private String filePath =  null;
 	
-    public Backup(){}
+    public Backup(){
+    
+    }
+    
+    public void elegirDirectorio(){
+    	JOptionPane.showMessageDialog(null, "Por favor elija la ubicación", "Verificar",JOptionPane.INFORMATION_MESSAGE);
+		 JFileChooser directorio = new JFileChooser();
+        int seleccion = directorio.showSaveDialog(new JPanel());
+        directorio.setDialogType(directorio.DIRECTORIES_ONLY);
+        if (seleccion == JFileChooser.APPROVE_OPTION){
+        	File f = directorio.getSelectedFile();
+	            filePath = f.getPath();
+	            if(!filePath.toLowerCase().endsWith(".sql"))
+	            {
+	                f = new File(filePath + ".sql");
+	                filePath = f.getPath();  
+	            }
+	            crearBackUpWindowsX86();
+        }
+    }
     
     public void crearBackUpWindowsX86(){
     	crearBackup("C:/Program Files/MySQL/MySQL Server 5.1/bin/","Falló el backup de windows de 32 bits");
@@ -38,44 +62,52 @@ public class Backup {
     }
     
 	public void cargarBackup() {
-		 JOptionPane.showMessageDialog(null, "Por favor elija la ubicación", "Verificar",JOptionPane.INFORMATION_MESSAGE);
-		 JFileChooser directorio = new JFileChooser();
-         directorio.setFileSelectionMode(JFileChooser.FILES_ONLY);
-         int seleccion = directorio.showOpenDialog(new JPanel());
-         directorio.setDialogType(directorio.DIRECTORIES_ONLY);
-         if (seleccion == JFileChooser.APPROVE_OPTION){
-         	File f = directorio.getSelectedFile();
-	            String filePath = f.getPath();
-	            try {
-		            // Ejecucion del cliente mysql
-		            Process p = Runtime.getRuntime().exec("mysql -u root -proot tesis");
-
-		            // Lectura de la salida de error y se muestra por pantalla.
-		            InputStream es = p.getErrorStream();
-		            muestraSalidaDeError(es);
-
-		            // Lectura del fichero de backup y redireccion a la entrada estandar
-		            // de mysql.
-		            OutputStream os = p.getOutputStream(); 
-		            FileInputStream fis = new FileInputStream(filePath);
-
-		            byte buffer[] = new byte[1024];
-		            int leido = fis.read(buffer);
-		            while (leido > 0) {
-		                System.out.println(leido);
-		                os.write(buffer, 0, leido);
-		                leido = fis.read(buffer);
-		            }
-		            os.close();
-		            fis.close();
-		            JOptionPane.showMessageDialog(null, "Se ha cargado el Back-Up con éxito", "Exito!",JOptionPane.PLAIN_MESSAGE);
-		        } catch (IOException e) {
-		            e.printStackTrace();
-		        }
-		        
-	            
-	        }          
-		 	    }
+	
+			 JOptionPane.showMessageDialog(null, "Por favor elija seleccione archivo.", "Verificar",JOptionPane.INFORMATION_MESSAGE);
+			 JFileChooser directorio = new JFileChooser();
+	         directorio.setFileSelectionMode(JFileChooser.FILES_ONLY);
+	         int seleccion = directorio.showOpenDialog(new JPanel());
+	         directorio.setDialogType(directorio.DIRECTORIES_ONLY);
+	         if (seleccion == JFileChooser.APPROVE_OPTION){
+	        	 int quitOption = JOptionPane.showConfirmDialog(new JFrame(),"Recuerde que al perderá aquella información almacenada \nposteriormente al último Back-Up realizado. \n¿Esta Seguro de cargar Back-Up?","Cargar Back-Up",JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
+	             if(quitOption==JOptionPane.YES_OPTION){
+		        	 try {
+		    			 eliminarDB();
+		    		 }
+		    		 catch (Exception e){
+		    			 System.out.println("No se encontró la base de datos!!");
+		    		 }
+		    		 finally{
+			         	File f = directorio.getSelectedFile();
+			         	filePath = f.getPath();
+			         	try {
+			         		crearDB();
+			         		// Ejecucion del cliente mysql
+			         		Process p = Runtime.getRuntime().exec("mysql -u root -proot tesis");
+			         		// Lectura de la salida de error y se muestra por pantalla.
+			         		InputStream es = p.getErrorStream();
+			         		muestraSalidaDeError(es);
+			         		// Lectura del fichero de backup y redireccion a la entrada estandar
+			         		// de mysql.
+			         		OutputStream os = p.getOutputStream(); 
+			         		FileInputStream fis = new FileInputStream(filePath);
+				            byte buffer[] = new byte[1024];
+				            int leido = fis.read(buffer);
+				            while (leido > 0) {
+				            	os.write(buffer, 0, leido);
+				            	leido = fis.read(buffer);
+				            }
+				            os.close();
+				            fis.close();
+				            JOptionPane.showMessageDialog(null, "Se ha cargado el Back-Up con éxito", "Exito!",JOptionPane.INFORMATION_MESSAGE);
+			         	} catch (Exception e) {	
+			         		e.printStackTrace();
+			         	}
+		           
+		    		 }    
+		             }
+	         }
+	}
 
 	 private void muestraSalidaDeError(final InputStream es) {
 	        Thread hiloError = new Thread() {
@@ -98,24 +130,13 @@ public class Backup {
 	
 	// METODO PARA REALIZAR EL BACKP
 	 public void crearBackup(String dirMySQL, String error){
-		 JOptionPane.showMessageDialog(null, "Por favor elija la ubicación", "Verificar",JOptionPane.INFORMATION_MESSAGE);
-		 JFileChooser directorio = new JFileChooser();
-         int seleccion = directorio.showSaveDialog(new JPanel());
-         directorio.setDialogType(directorio.DIRECTORIES_ONLY);
-         if (seleccion == JFileChooser.APPROVE_OPTION){
-         	File f = directorio.getSelectedFile();
-	            String filePath = f.getPath();
-	            if(!filePath.toLowerCase().endsWith(".sql"))
-	            {
-	                f = new File(filePath + ".sql");
-	                filePath = f.getPath();  
-	            }
+		 
 
 	            try{       
 	                //sentencia para crear el BackUp
 	                 Process run = Runtime.getRuntime().exec(dirMySQL+"mysqldump --host=localhost --port=3306 --user=root --password=root --compact --complete-insert --extended-insert --skip-quote-names"
 	                		 +" --skip-comments --skip-triggers tesis");
-	                 
+	              
 	                //se guarda en memoria el backup
 	                InputStream in = run.getInputStream();
 	                BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -138,12 +159,16 @@ public class Backup {
 	                fichero = new FileWriter(backupFile);
 	                pw = new PrintWriter(fichero);                                         
 	                pw.println(temp.toString());  
-	                JOptionPane.showMessageDialog(null, "Se ha creado el Back-Up con éxito", "Exito!",JOptionPane.PLAIN_MESSAGE);
+	                JOptionPane.showMessageDialog(null, "Se ha creado el Back-Up con éxito", "Exito!",JOptionPane.INFORMATION_MESSAGE);
 
 	           }
 	            catch (Exception ex){
+	            	ex.printStackTrace();
 	            	System.out.println(error);
-	            	crearBackUpWindowsX64();
+	            	if(error.compareTo("Falló el backup de windows de 64 bits")!=0){
+	            		crearBackUpWindowsX64();
+	            	}
+	            	
 	            } finally {
 	            	try {           
 	            	   if (null != fichero)
@@ -152,7 +177,63 @@ public class Backup {
 	                   e2.printStackTrace();
 	               }
 	            }   
-         }          
+                   
 	 }  
-	
+	 
+	 public void eliminarDB() throws Exception {
+			Statement stmt;
+			
+			        //Register the JDBC driver for MySQL.
+			        Class.forName("com.mysql.jdbc.Driver");
+
+			        //Define URL of database server for
+			        // database named mysql on the localhost
+			        // with the default port number 3306.
+			        String url = "jdbc:mysql://localhost:3306/mysql";
+
+			        //Get a connection to the database for a
+			        // user named root with a blank password.
+			        // This user is the default administrator
+			        // having full privileges to do anything.
+			        Connection con = (Connection) DriverManager.getConnection(url,"root", "root");
+
+			        //Display URL and connection information
+			        System.out.println("URL: " + url);
+			        System.out.println("Connection: " + con);
+
+			        //Get a Statement object
+			        stmt = (Statement) con.createStatement();
+			        stmt.executeUpdate("DROP DATABASE tesis");
+			        con.close();
+	 
+	 }
+	 
+	 public void crearDB() throws Exception {
+			Statement stmt;
+			
+			        //Register the JDBC driver for MySQL.
+			        Class.forName("com.mysql.jdbc.Driver");
+
+			        //Define URL of database server for
+			        // database named mysql on the localhost
+			        // with the default port number 3306.
+			        String url = "jdbc:mysql://localhost:3306/mysql";
+
+			        //Get a connection to the database for a
+			        // user named root with a blank password.
+			        // This user is the default administrator
+			        // having full privileges to do anything.
+			        Connection con = (Connection) DriverManager.getConnection(url,"root", "root");
+
+			        //Display URL and connection information
+			        System.out.println("URL: " + url);
+			        System.out.println("Connection: " + con);
+
+			        //Get a Statement object
+			        stmt = (Statement) con.createStatement();
+			        stmt.executeUpdate("CREATE DATABASE tesis");
+
+			        con.close();
+		 
+	 }
 }
