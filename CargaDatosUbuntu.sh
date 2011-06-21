24ac6b9dfd26f452481c0dff6d92efc961d8472a
marmolleonardo <marmolleonardo@gmail.com>
2011-06-21 21:17:55 +0000

    CargaDatosUbuntu.sh---- EnhanceUbuntu.sh----- RunUbuntu.sh-----

---
+++ CargaDatosUbuntu.sh
@@ -0,0 +1,1 @@
+java -cp ./lib/jdo2-api-2.3-eb.jar:./lib/datanucleus-core-2.2.0-m1.jar:./lib/datanucleus-rdbms-2.2.0-m1.jar:./lib/log4j-1.2.7.jar:./lib/mysql-connector-java-5.1.12-bin.jar:./classes/:.  comun.CargaDatos
