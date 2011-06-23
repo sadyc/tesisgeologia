ae2040b58c20a6d1f2c4756751371c5377d51b2f
marmolleonardo@gmail.com <marmolleonardo@gmail.com@ef049f63-3810-13be-70b6-c2ffdda8e0be>
2011-06-23 20:57:35 +0000

    NO ANDA el reeporte ubuntero

--- trunk/src/cuReporte/report/MakeReport.java
+++ trunk/src/cuReporte/report/MakeReport.java
@@ -9,13 +9,25 @@ import net.sf.jasperreports.engine.JasperFillManager;
 public class MakeReport {
 	
 	public static String BASE = (new java.io.File("")).getAbsolutePath(); 
-	public static String PATH_SOURCE_REPORT = BASE + "\\src/cuReporte/report/xml/";
+	public static String PATH_SOURCE_REPORT = BASE + "/src/cuReporte/report/xml/";
  
-	public static String PATH_OUTPUT_REPORT = BASE + "/classes/cuReporte/report/xml";
+	public static String PATH_OUTPUT_REPORT = BASE + "/classes/cuReporte/report/xml/";
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void make(String nameReport) {
 		try {
+			
+			String osName = System.getProperty("os.name" );
+			System.out.println(osName);
+            if (osName.equals("Linux")) {
+                    PATH_SOURCE_REPORT = BASE + "/src/cuReporte/report/xml/";
+                    PATH_OUTPUT_REPORT = BASE + "/classes/cuReporte/report/xml/";
+            }
+            else {
+                    PATH_SOURCE_REPORT = BASE + "\\src\\cuReporte\\report\\xml\\";
+                    PATH_OUTPUT_REPORT = BASE + "\\classes\\cuReporte\\report\\xml\\";
+                    
+            }
 			System.setProperty("org.xml.sax.driver","org.apache.xerces.parsers.SAXParser");
 			String fileXML = PATH_SOURCE_REPORT + nameReport + ".xml";
 			String fileJASPER = PATH_OUTPUT_REPORT + nameReport + ".jasper";
@@ -30,7 +42,7 @@ public class MakeReport {
 			System.out.println("Compiling time : "+ (System.currentTimeMillis() - start) + "\n");
 			System.out.println("Fill Report to File : " + fileJRPRINT);
 			HashMap directorio = new HashMap();
-			directorio.put("pathSistema", BASE);
+			directorio.put("pathSistema", BASE);//  ACA TA LA COSA FEA
 			JasperFillManager.fillReportToFile(fileJASPER, fileJRPRINT, directorio,new JREmptyDataSource());
 			System.out.println("Filling time : "+ (System.currentTimeMillis() - start) + "\n");
 
